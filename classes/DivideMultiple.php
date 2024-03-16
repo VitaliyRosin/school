4d8a003d2d012863500ab98e9d81b42341034174
Vitaliy Rosin <rosinsskii@gmail.com>
2024-03-16 14:00:50 +0200

    added lesson_7

---
+++ classes/DivideMultiple.php
@@ -0,0 +1,43 @@
+<?php
+
+// 3. Создать по 2 наследника от наследников первого уровня. Каждое свойство должно иметь геттер и сеттер.
+// Наследники должны реализовать по одному методу который выполняет одно математическое действие с данными родителя
+// и своими данными. И по одному методу который выполняет любое математическое действие со свойством корневого класса
+// и своим свойством. В случае если реализован наследник класса содержащего абстрактную функцию
+// то класс должен содержать реализацию абстракции.
+
+class DivideMultiple extends PLus
+{
+
+    private int $argumentFour = 0;
+
+    /**
+     * @return int
+     */
+    public function getArgumentFour(): int
+    {
+        return $this->argumentFour;
+    }
+
+    /**
+     * @param int $argumentFour
+     */
+    public function setArgumentFour(int $argumentFour): void
+    {
+        $this->argumentFour = $argumentFour;
+    }
+
+    // В наследнике реализован метод, который выполняет одно математическое действие с данными родителя
+    // и своими данными.
+    public function divideMultipleFunction(): int
+    {
+        return parent::getArgumentTree() / 2 - $this->getArgumentFour();
+    }
+
+    // В наследнике реализован метод, который выполняет одно математическое действие со свойством корневого класса
+    // и своим свойством
+    public function divideMultipleCalcFunction(): int
+    {
+        return Calculate::getArgumentOne() / 2 - $this->getArgumentFour();
+    }
+}
---
+++ classes/MinusPlus.php
@@ -0,0 +1,43 @@
+<?php
+
+// 3. Создать по 2 наследника от наследников первого уровня. Каждое свойство должно иметь геттер и сеттер.
+// Наследники должны реализовать по одному методу который выполняет одно математическое действие с данными родителя
+// и своими данными. И по одному методу который выполняет любое математическое действие со свойством корневого класса
+// и своим свойством. В случае если реализован наследник класса содержащего абстрактную функцию
+// то класс должен содержать реализацию абстракции.
+
+class MinusPLus extends PLus
+{
+
+    private int $argumentFour = 0;
+
+    /**
+     * @return int
+     */
+    public function getArgumentFour(): int
+    {
+        return $this->argumentFour;
+    }
+
+    /**
+     * @param int $argumentFour
+     */
+    public function setArgumentFour(int $argumentFour): void
+    {
+        $this->argumentFour = $argumentFour;
+    }
+
+    // В наследнике реализован метод, который выполняет одно математическое действие с данными родителя
+    // и своими данными.
+    public function minusPlusFunction(): int
+    {
+        return parent::getArgumentTree() - $this->getArgumentFour();
+    }
+
+    // В наследнике реализован метод, который выполняет одно математическое действие со свойством корневого класса
+    // и своим свойством
+    public function minusPlusCalcFunction(): int
+    {
+        return Calculate::getArgumentOne() - $this->getArgumentFour();
+    }
+}
