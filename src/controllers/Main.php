8b23f1b06b8d39e4a7a63272660e2ad3e7758d5c
VitaliyRosin <160955613+VitaliyRosin@users.noreply.github.com>
2024-04-07 13:20:19 +0300

    Update Main.php

--- src/controllers/Main.php
+++ src/controllers/Main.php
@@ -10,11 +10,12 @@ class Main
 {
     public function __construct(array $url){
         $model = new Posts();
-        //$connection = $model->connect();
-        //$sql = $connection->query('SELECT * FROM `user`');
-        //$sql->execute();
-        //$result = $sql->fetchAll($connection::FETCH_ASSOC);
-        //var_dump($result);
+        $connect = new Connect();
+        $connection = $connect->getConnect();
+        $sql = $connection->query('SELECT * FROM `user`');
+        $sql->execute();
+        $result = $sql->fetchAll($connection::FETCH_ASSOC);
+        var_dump($result); // так все работает
         $data['posts'] = $model->findAll();
         Viewer::view($url, $data);
     }
